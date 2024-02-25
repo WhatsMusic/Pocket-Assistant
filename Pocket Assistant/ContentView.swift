1748627eeed91cc1bbccdd5caa9064e705f26d62
WhatsMusic.de <info@whatsmusic.de>
2024-02-25 22:51:44 +0100

    Initial Commit

---
+++ Pocket Assistant/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pocket Assistant
+//
+//  Created by Robert Schulz on 25.02.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pocket Assistant/Pocket_AssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pocket_AssistantApp.swift
+//  Pocket Assistant
+//
+//  Created by Robert Schulz on 25.02.24.
+//
+
+import SwiftUI
+
+@main
+struct Pocket_AssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
